3ffb5f5147673b8292a803744461afa8b1b96c39
oscarGR-123 <j.oscar.rf.223@gmail.com>
2025-03-20 08:01:30 -0600

    Add files via upload

---
+++ newfile5.cxx
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    double r = 5;
+    double pi = 3.1416;
+    double area = pi * r * r;
+    printf("%f", area);
+    printf ("\n");
+    system("pause");
+    return 0;
